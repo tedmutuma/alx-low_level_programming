a026c965ea4245172e3455736a45233bc2d02ff1
Ted <tm.mutuma@gmail.com>
2022-09-08 13:14:58 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- *main-printing value of put
- *
- *Description: use the main function
- *the program prints Programming is like building a multilingual puzzle
- *return: 0
+
+ * main - main block
+
+ * Return: 0
+
  */
 int main(void)
 {
