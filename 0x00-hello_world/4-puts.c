0dafcce2d3d08979285ac2287968f6b0962a74e5
Ted <tm.mutuma@gmail.com>
2022-09-08 12:20:29 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  *return(0)
  */
-int main (void)
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
