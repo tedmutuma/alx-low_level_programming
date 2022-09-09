229e5d754f9b241df06a4e9b43d3287f9d18ebc0
Ted <tm.mutuma@gmail.com>
2022-09-09 13:05:18 -0700

    printing combination of two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-
 	int n1 = 48;
 
 	int a = 0;
@@ -18,48 +17,25 @@ int main(void)
 
 	int com = 44;
 
-
-
 	while (a <= 99)
-
 	{
-
 		b = a + 1;
-
 		while (b <= 99)
-
 		{
-
 			putchar((a / 10) + n1);
-
 			putchar((a % 10) + n1);
-
 			putchar(32);
-
 			putchar((b / 10) + n1);
-
 			putchar((b % 10) + n1);
-
 			if (a != 98 || b != 99)
-
 			{
-
 				putchar(com);
-
 				putchar(32);
-
 			}
-
 			b += 1;
-
 		}
-
 		a += 1;
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
