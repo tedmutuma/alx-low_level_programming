31bb9b6752f90b38c5f533656b23e82006e37fb4
Ted <tm.mutuma@gmail.com>
2022-09-14 12:28:29 -0700

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - main block
+ *print_alphabet - print alphabets
+ *
+ *Description: print_alphabet use to print a to z
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char i = 'a';
+
+
+
+	while (i <= 'z')
+
+	{
+
+		_putchar(i);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+}
