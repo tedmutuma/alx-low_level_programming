864275fa25348483dc0e942193ecc97349f745de
Opiomwema <100408799+Opiomwema@users.noreply.github.com>
2022-09-15 22:57:21 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,30 +1,20 @@
-#include <stdio.h>
-/**
- * main - print out numbers from 1 to 100 with logic
- * Return: return integer value
- */
-int main(void)
-{
-	int i;
+#include "main.h"
 
-	for (i = 1; i <= 100; i++)
+/**
+ * print_number - prints an interger.
+ *
+ * @n: number.
+ */
+void print_number(int n)
+{
+	if (n < 0)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("%s", "FizzBuzz");
-		} else if (i % 3 == 0)
-		{
-			printf("%s", "Fizz");
-		} else if (i % 5 == 0)
-		{
-			printf("%s", "Buzz");
-		} else
-		{
-			printf("%d", i);
-		}
-		if (i < 100)
-			printf("%c", ' ');
+		_putchar('-');
+		n = -n;
 	}
-	printf("%c", '\n');
-	return (0);
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
