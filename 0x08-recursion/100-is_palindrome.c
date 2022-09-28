38f451773741abe9c308c152c857c8e689f5de5e
Ted <tm.mutuma@gmail.com>
2022-09-28 12:34:48 -0700

    palindome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,8 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(s + 1));}
+		return (1 + _strlen_recursion(s + 1));
+}
 /**
  * comparator - compares each character of the string.
  * @s: string
@@ -23,7 +24,8 @@ int comparator(char *s, int n1, int n2)
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));}
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
 	return (0);
 }
 /**
